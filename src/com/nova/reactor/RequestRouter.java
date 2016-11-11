246fdf08dcc63e0498afdeea5e56446d546152aa
BSteffaniak <BradenSteffaniak@gmail.com>
2016-11-10 18:59:32 -0800

    Updated to return access token.

--- src/com/nova/reactor/RequestRouter.java
+++ src/com/nova/reactor/RequestRouter.java
@@ -5,9 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.net.ssl.HttpsURLConnection;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 public class RequestRouter
@@ -22,7 +26,7 @@ public class RequestRouter
 	
 	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "/auth/github/callback")
-	public String loginUser(@RequestParam String code) throws SQLException, IOException
+	public String loginUser(@RequestParam String code, HttpServletResponse response) throws SQLException, IOException
 	{
 		String url = "https://github.com/login/oauth/access_token";
 		URL obj = new URL(url);
@@ -50,28 +54,45 @@ public class RequestRouter
 		BufferedReader in = new BufferedReader(
 			new InputStreamReader(con.getInputStream()));
 		String inputLine;
-		StringBuffer response = new StringBuffer();
+		StringBuffer r = new StringBuffer();
 		
 		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
+			r.append(inputLine);
 		}
 		in.close();
 		
-		System.out.println("Access codE: " + response);
+		System.out.println("Access codE: " + r);
+		
+		Map<String, String> values = splitQuery(r.toString());
+		
+		response.setHeader("access_token", values.get("access_token"));
 		
 		return
 			"<html>\n" +
 			"<body>\n" +
 			"	success... closing window.\n\n" +
 			"	<script>\n" +
-			"		var myVariable = 'hello??';\n" +
 			"		if (opener && \"\" != opener.location) {\n" +
-			"			opener.handler(myVariable);\n" +
+			"			opener.handler();\n" +
 			"		}\n" +
 			"		window.close();\n" +
 			"	</script>\n" +
 			"</body>\n" +
 			"</html>";
 		//return new GithubAuthResponse();
+	}
+	
+	public static Map<String, String> splitQuery(String query) throws UnsupportedEncodingException
+	{
+		Map<String, String> query_pairs = new LinkedHashMap<>();
+		String[] pairs = query.split("&");
+		
+		for (String pair : pairs)
+		{
+			int idx = pair.indexOf("=");
+			query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+		}
+		
+		return query_pairs;
 	}
 }

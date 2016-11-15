22b1f2a9fbabdf7b4061ccdad09974e0c2b6f43a
BSteffaniak <BradenSteffaniak@gmail.com>
2016-11-14 17:02:20 -0800

    Updated to do all querying in single function. Added build function stub.

--- src/com/nova/reactor/models/BuildRepo.java
+++ src/com/nova/reactor/models/BuildRepo.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class BuildRepo
@@ -15,16 +16,31 @@ public class BuildRepo
 	@Autowired
 	JdbcTemplate connection;
 	
+	public Build[] getBuilds(String repo)
+	{
+		return getBuilds(repo, null);
+	}
+	
 	public Build[] getBuilds(String repo, String commit)
 	{
 		final ArrayList<Build> builds = new ArrayList<>();
 		
-		Object[] params = new Object[] {
-			repo.toLowerCase(),
-			commit.toLowerCase()
-		};
+		ArrayList paramsList = new ArrayList();
 		
-		connection.query("SELECT * FROM repo_data.builds WHERE LOWER(repo)=? AND LOWER(commit)=?", params, (ResultSetExtractor)x -> {
+		paramsList.add(repo.toLowerCase());
+		
+		String extra = "";
+		
+		if (commit != null)
+		{
+			paramsList.add(commit.toLowerCase());
+			
+			extra = "AND LOWER(commit)=?";
+		}
+		
+		Object[] params = paramsList.toArray(new Object[0]);
+		
+		connection.query("SELECT * FROM repo_data.builds WHERE LOWER(repo)=? " + extra, params, (ResultSetExtractor)x -> {
 			if (x.next())
 			{
 				ArrayList<HashMap<String, Object>> maps = DataUtils.resultSetToArrayList(x);
@@ -41,28 +57,18 @@ public class BuildRepo
 		return builds.toArray(new Build[0]);
 	}
 	
-	public Build[] getBuilds(String repo)
+	public Build build(String repo, String commit)idk
 	{
-		final ArrayList<Build> builds = new ArrayList<>();
+		/*Map<String, Object> keys = DataUtils.keysFromUpdate(connection,
+			"INSERT INTO users.users(username, full_name, password, email, phone_number) VALUES(?, ?, digest(digest(?, 'sha256'), 'sha256'), ?, ?)",
+			request.getUsername(), request.getFullName(), request.getPassword(), request.getEmail(), request.getPhoneNumber());
 		
-		Object[] params = new Object[] {
-			repo.toLowerCase()
-		};
+		connection.update("INSERT INTO users.profiles(user_id) VALUES(?)", keys.get("id"));
 		
-		connection.query("SELECT * FROM repo_data.builds WHERE LOWER(repo)=?", params, (ResultSetExtractor)x -> {
-			if (x.next())
-			{
-				ArrayList<HashMap<String, Object>> maps = DataUtils.resultSetToArrayList(x);
-				
-				for (int i = 0; i < maps.size(); i++)
-				{
-					builds.add(new Build(maps.get(i)));
-				}
-			}
-			
-			return null;
-		});
+		response.setSuccess(keys.size() > 0);
 		
-		return builds.toArray(new Build[0]);
+		return response;*/
+		
+		return null;
 	}
 }
